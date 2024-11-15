a78496ace72db034e9dce22219b4d0c0c5ce5b7b
Diagorn <mikhail.gasin@fors.ru>
2024-11-15 23:07:57 +0300

    added appearance animations

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -10,35 +10,49 @@ export default defineComponent({
         {text: 'Играть', path: '/game'},
         {text: 'Правила', path: '/rules'},
         {text: 'Про игру', path: '/about'}
-      ]
+      ],
+      bannerVisible: false,
+      headerVisible: false,
+      buttonsVisible: false,
     }
   },
   methods: {
     redirect(to) {
       this.$router.push(to)
     }
+  },
+  mounted() {
+    setTimeout(() => this.bannerVisible = true, 200)
+    setTimeout(() => this.headerVisible = true, 600)
+    setTimeout(() => this.buttonsVisible = true, 1000)
   }
 })
 </script>
 
 <template>
   <div class="image-container">
-    <img class="banner" src="../assets/screens/semerochka_banner.png" alt="">
+    <Transition :duration="1000" name="slide">
+      <img class="banner" src="../assets/screens/semerochka_banner.png" alt="" v-if="bannerVisible">
+    </Transition>
   </div>
 
-  <p class="text-center text-white mt-7 header">
-    Сможешь ли ты выстоять?
-  </p>
+  <Transition :duration="1000" name="slide">
+    <p class="text-center text-white mt-7 header" v-if="headerVisible">
+      Сможешь ли ты выстоять?
+    </p>
+  </Transition>
 
-  <div class="buttons-container mt-5">
-    <app-btn
-      v-for="(item, index) in navigation"
-      :key="index"
-      :text="item.text"
-      @click="redirect(item.path)"
-    >
-    </app-btn>
-  </div>
+  <Transition :duration="1000" name="slide">
+    <div class="buttons-container mt-5" v-if="buttonsVisible">
+      <app-btn
+          v-for="(item, index) in navigation"
+          :key="index"
+          :text="item.text"
+          @click="redirect(item.path)"
+      >
+      </app-btn>
+    </div>
+  </Transition>
 </template>
 
 <style scoped>
@@ -71,6 +85,43 @@ export default defineComponent({
 .header {
   margin: 5vh 0;
   font-size: 36px;
+}
+
+.slide-enter-active, .slide-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+
+/* delay leave of parent element */
+.slide-leave-active {
+  transition-delay: 0.25s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateY(30px);
+  opacity: 0;
+}
+
+/* we can also transition slide elements using slide selectors */
+.slide-enter-active .inner,
+.slide-leave-active .inner {
+  transition: all 0.3s ease-in-out;
+}
+
+/* delay enter of slide element */
+.slide-enter-active .inner {
+  transition-delay: 0.25s;
+}
+
+.slide-enter-from .inner,
+.slide-leave-to .inner {
+  transform: translateX(30px);
+  /*
+  	Hack around a Chrome 96 bug in handling slide opacity transitions.
+    This is not needed in other browsers or Chrome 99+ where the bug
+    has been fixed.
+  */
+  opacity: 0.001;
 }
 
 </style>
