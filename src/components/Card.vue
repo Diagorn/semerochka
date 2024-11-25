36b4a37142eb2a4fbed85f9781d9fc2016e1674c
Diagorn <mikhail.gasin@fors.ru>
2024-11-25 17:00:43 +0300

    Added hint to flipped card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -12,6 +12,7 @@ export default defineComponent({
         transform: 'rotateY(0deg)'
       },
       isFlipping: false,
+      hintTextVisible: false,
     }
   },
   props: {
@@ -22,7 +23,7 @@ export default defineComponent({
   },
   methods: {
     getBackFile,
-    flipCard(e) {
+    flipCard() {
       if (this.isFlipping) {
         return
       }
@@ -75,6 +76,15 @@ export default defineComponent({
     },
     getCardSrc() {
       return `${this.folderPath}/${this.card.getFilePath()}`
+    },
+    showHint() {
+      this.hintTextVisible = true
+    },
+    hideHint() {
+      this.hintTextVisible = false
+    },
+    toggleHintVisibility() {
+      this.hintTextVisible = !this.hintTextVisible
     }
   }
 })
@@ -87,6 +97,16 @@ export default defineComponent({
       :style="cardStyle"
       v-if="this.card"
   >
+    <div
+        class="hint"
+        v-if="card.isFlipped()"
+        @mouseenter="showHint"
+        @mouseleave="hideHint"
+        @click.stop="toggleHintVisibility"
+    >
+      <span class="material-icons hint-icon">question_mark</span>
+      <div class="hint-text" v-if="hintTextVisible"> {{ card.getCaption() }} </div>
+    </div>
     <img
         id="front"
         v-if="this.card && this.card.isFlipped()"
@@ -120,6 +140,60 @@ export default defineComponent({
   border: var(--card-inner-border) solid var(--white);
   box-shadow: 0 0 0 var(--card-outer-border) var(--black);
 }
+
+.hint {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  top: 20px;
+  right: 0;
+
+  border-radius: 50%;
+  background: var(--light-blue);
+
+  cursor: pointer;
+}
+
+.hint-icon {
+  color: var(--white);
+}
+
+.hint-text {
+  color: var(--white);
+  visibility: hidden;
+  width: 140px;
+  background-color: var(--deep-blue);
+  text-align: center;
+  border-radius: 5px;
+  padding: 5px;
+  position: absolute;
+  z-index: 1;
+  top: 125%;
+  left: 50%;
+  margin-left: -75px;
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.hint-text::after {
+  content: "";
+  position: absolute;
+  bottom: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: var(--black) transparent transparent transparent;
+}
+
+.hint:hover .hint-text {
+  visibility: visible;
+  opacity: 1;
+}
+
 
 .no-select {
   -webkit-user-select: none; /* Safari */
